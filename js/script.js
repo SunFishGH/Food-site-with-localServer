69e8457848a9c65594a26ebbe64593ee2936a629
Denis <frontendsunfish@gmail.com>
2021-08-08 15:26:04 +0300

    lesson 49 Pest operator and defolt parametrs

--- js/script.js
+++ js/script.js
@@ -148,12 +148,13 @@ window.addEventListener('DOMContentLoaded', () =>{
     // Используем классы для карточек
 
     class MenuCard{
-        constructor(src,alt,title,descr,price,parentSelector){
+        constructor(src,alt,title,descr,price,parentSelector, ...classes){
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 73;
             this.changeToRUB();
@@ -165,8 +166,15 @@ window.addEventListener('DOMContentLoaded', () =>{
 
         render(){
             const element = document.createElement('div');
+            
+            if(this.classes.length === 0){
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -175,7 +183,6 @@ window.addEventListener('DOMContentLoaded', () =>{
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
             `;
             this.parent.append(element);
         }
@@ -187,7 +194,9 @@ window.addEventListener('DOMContentLoaded', () =>{
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         10,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big'
     ).render();
 
     new MenuCard(
@@ -196,7 +205,8 @@ window.addEventListener('DOMContentLoaded', () =>{
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         15,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -205,7 +215,8 @@ window.addEventListener('DOMContentLoaded', () =>{
         'Меню "Постное"',
         'Меню "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         18,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
 
